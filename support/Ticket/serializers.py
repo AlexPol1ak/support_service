d792457d6ae0774c69cef2d249f553faf144d1b6
Alex <zarjrw1994@gmail.com>
2022-11-16 16:48:26 +0300

    Added serializers for creating and reading comments.

--- support/Ticket/serializers.py
+++ support/Ticket/serializers.py
@@ -2,8 +2,7 @@ from django.contrib.auth.hashers import make_password
 
 from rest_framework import serializers
 
-from Ticket.models import Ticket
-
+from Ticket.models import Ticket, Comments
 
 
 # Alternative option
@@ -29,19 +28,67 @@ class CreateTicketSerializer(serializers.ModelSerializer):
     """Serializer for creating a ticket"""
 
     class Meta:
+
         model = Ticket
         fields = ('user_id', 'title', 'user_message',)
 
 
 class GetUsersTiketsSerializer(serializers.ModelSerializer):
-    """A serializer for retrieving all of the user's tickets."""
+    """A serializer for retrieving all the user's tickets."""
+
     class Meta:
+
         model = Ticket
-        fields = ('id', 'user_id', 'title', 'user_message', 'message_date')
+        fields = ('id', 'user_id', 'title', 'message_date')
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    """Serialization of all commentaries"""
+
+    class Meta:
+
+        model = Comments
+        fields = '__all__'
+
 
 class DetailTicketSerializer(serializers.ModelSerializer):
     """Serializer for detailed display of ticket data"""
+
+    # tickets = serializers.StringRelatedField(many=True)
+    comments = CommentsSerializer(many=True, read_only=True)
+    class Meta:
+
+        model = Ticket
+        fields = ['user_id', 'title', 'user_message', 'message_date', 'support_id', 'support_response',
+                  'reply_date', 'frozen', 'resolved', 'resolved_date', 'comments']
+
+
+class CreateCommnetSerilizer(serializers.ModelSerializer):
+    """Serializer for creating user comments to a ticket."""
+
+    ticket_id = serializers.IntegerField(write_only=True)
+    class Meta:
+
+        model = Comments
+        fields = ('ticket_id', 'user_comment')
+
+    def validate_ticket_id(self, value):
+        try:
+            ticket = Ticket.objects.get(id=value)
+        except:
+            raise serializers.ValidationError(f"Ticket {value} not found")
+
+        if self.context['request'].user.pk == ticket.user_id:
+            return value
+        else:
+            raise serializers.ValidationError(f"The user is not the author of the appeal and can not leave comments")
+
+
+class GetAllTicketsSerilizers(serializers.ModelSerializer):
+
     class Meta:
         model = Ticket
-        fields = "__all__"
+        fields = ['user_id', 'title', 'message_date', 'support_id',
+                  'frozen', 'resolved', 'resolved_date',
+                  ]
 
