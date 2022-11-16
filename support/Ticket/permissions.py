c9cf723d75f643ebc13982343de368aca4b550b7
Alex <zarjrw1994@gmail.com>
2022-11-16 16:47:26 +0300

    Added restrictions on access to the ticket and comments to the ticket. Access is allowed to the author if the ticket is not closed and not frozen.

--- support/Ticket/permissions.py
+++ support/Ticket/permissions.py
@@ -5,5 +5,22 @@ class IsAuthorsObjectOrSupport(permissions.BasePermission):
     """Allows access only to the author or support."""
 
     def has_object_permission(self, request, view, obj):
+        # obj - instance model Ticket
         if obj.user_id == request.user.id or request.user.is_support:
             return bool(True)
+
+class TicketNotFrozenAndClosed(permissions.BasePermission):
+    """Provides access if the ticket is not frozen and closed."""
+
+    def has_object_permission(self, request, view, obj):
+        # obj - instance model Ticket
+        if not obj.frozen and not obj.frozen:
+            return bool(True)
+
+
+class IsOwner(permissions.BasePermission):
+    """Provides access to the author"""
+
+    def has_object_permission(self, request, view, obj):
+        # obj - instance model Ticket
+        return bool(obj.user_id == request.user.id)
