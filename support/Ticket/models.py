48ee3a63eac0bdb0e29e090226c07f1dddabf26c
Alex <zarjrw1994@gmail.com>
2022-11-16 16:44:58 +0300

    Added a line of model documentation. Removed the "is_comment" field in the Ticket model. In the Comment model the field support_id has been added, it will contain the id of the support that replied to the comment.

--- support/Ticket/models.py
+++ support/Ticket/models.py
@@ -2,6 +2,8 @@ from django.core.validators import MinLengthValidator
 from django.db import models
 
 class Ticket(models.Model):
+    """The model of a user's call to support."""
+
     user_id = models.IntegerField() # automatically in the user serializer
     title = models.CharField(max_length=45 ,validators=[MinLengthValidator(4)])
     user_message = models.TextField(max_length=500, validators=[MinLengthValidator(4)])
@@ -12,21 +14,21 @@ class Ticket(models.Model):
     frozen = models.BooleanField(default=False)
     resolved = models.BooleanField(default=False)
     resolved_date = models.DateTimeField(null=True) # automatically in the support serializer
-    is_comment = models.BooleanField(default=False) # automatically in the support serializer
 
     def __str__(self):
         st = f"User- {self.user_id}.Frozen: {self.frozen}. Resolved: {self.resolved}"
         return st
 
-# from Ticket.models import *
-# t = Ticket.objects.create(user_id=4, title="new title", user_message='new message')
 
 class Comments(models.Model):
+    """Model of comments on a ticket."""
+
     user_comment = models.TextField(max_length=500,validators=[MinLengthValidator(4)])
     comment_date = models.DateTimeField(auto_now_add=True)
     support_response = models.TextField(max_length=500, blank=True, validators=[MinLengthValidator(4)])
     reply_date = models.DateTimeField(null=True)
-    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    support_id = models.IntegerField(null=True)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self):
         return str(self.ticket)
