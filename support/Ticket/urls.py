5f1d358df9a405b74d2bcf50c1de364a0929c177
Alex <zarjrw1994@gmail.com>
2022-11-16 16:50:00 +0300

    Added routes for detailed display of the ticket, creation of comments by user. Displaying of all tickets for support.

--- support/Ticket/urls.py
+++ support/Ticket/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('test/', views.ticket_test, name='ticket_test'),
     path('create-ticket/', views.CreateTicketAPIView.as_view(), name='create_ticket'),
     path('my-tickets/', views.GetUsersTiketsAPIView.as_view(), name='get_users_tickets'),
-    path('ticket/<int:pk>/', views.DetailTicketAPIView.as_view(), name='detail_ticket')
+    path('ticket/<int:pk>/', views.DetailTicketAPIView.as_view(), name='detail_ticket'),
+    path('add-comment/', views.CreateCommentAPIView.as_view(), name='add_comment'),
+    path('get-all-tickets/', views.GetAllTicketsAPIView.as_view(), name='get_all_tickets'),
 ]
